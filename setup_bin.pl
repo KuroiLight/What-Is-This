984e3b9bace86e0ced46f4f3265ed91cb77ae612
KuroiLight <Kuroilight@openmailbox.org>
2013-09-10 21:48:37 -0700

    added make executable to setup_bin.pl

--- setup_bin.pl
+++ setup_bin.pl
@@ -1,22 +1,22 @@
 #!/usr/bin/perl
 
-#use Cwd qw/ abs_path /;
-my $abs_path = $ENV{'PWD'};
+use Cwd qw/ abs_path /;
 use autodie;
 use File::Temp qw/ tempfile /;
 use File::Copy qw/ copy /;
-
+use Env qw/ PATH PWD /;
+my $abso_path = abs_path('');
 #########
 # wit installer script [WIP]
 #########
-
+my $script_name = 'wit';
 my $install = 0;
 my $uninstall = 0;
 
 my $ISROOT = (($> + $<) ? 0 : 1);
 
-my $wit_bin = '/usr/bin/wit';
-my $wit_path = $abs_path . '/wit.pl';
+my $wit_bin = '/usr/bin/' . $script_name;
+my $wit_path = $abso_path . '/' . $script_name . '.pl';
 
 foreach my $arg (@ARGV) {
     if($arg =~ qr/u/i) {
@@ -24,7 +24,7 @@ foreach my $arg (@ARGV) {
     } elsif ($arg =~ qr/i/i) {
         $install = 1;
     } elsif ($arg =~ qr/h/i) {
-        print "This script will symlink wit.pl to /usr/bin/wit.\nThis script must be run as root.";
+        print "This script will symlink $script_name.pl to /usr/bin/$script_name.\nThis script must be run as root.";
         print "\n\t-u\t uninstall link from bin directory";
         print "\n\t-i\t install link to bin directory";
         print "\n\t-h\t display this help\n";
@@ -61,6 +61,7 @@ sub Install {
     }
     print "symlinking...\n";
     symlink ($wit_path, $wit_bin) or warn "could not link file..\n";
+	system("chmod +x $wit_path"); #make executable
 }
 
 if(not -e -r $wit_path) {
