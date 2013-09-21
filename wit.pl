b37e160cf383758e127423d0f22ad482ba13a988
KuroiLight <Kuroilight@openmailbox.org>
2013-09-21 12:17:04 -0700

    minor code reduction and replacements

--- wit.pl
+++ wit.pl
@@ -23,7 +23,7 @@ eval {
 };
 
 
-my $wit_version = '0.42.2';
+my $wit_version = '0.42.3';
 
 my @bins = sort 
 '/usr/local/bin',
@@ -78,7 +78,9 @@ sub ReadFile ($) { #pass (file)
     return do {
         local $/ = undef;
         my $handle = OpenFile($_[0]);
-        return ($handle ? scalar <$handle> : undef);
+        my $contents = <$handle>;
+        close($handle);
+        return $contents;
     };
 }
 
@@ -100,16 +102,6 @@ sub Startup { #init code here
             print "\n\t-ac,--altcolors\t\tuses alternate color scheme";
             print "\n\t-nc,--nocolors \t\tturns off colors completely\n";
             exit 0;
-        } elsif($arg =~ /(-i|--install)/){ #start hackish code:
-            my $abs_path = $ENV{'PWD'};
-            print((not (-e ($abs_path . '/setup_bashrc.pl')) and "setup script missing...\n")
-            or ((not system('perl ' . $abs_path . '/setup_bashrc.pl -i')) or "setup failed\n")); #extra parenthesis help get the point across
-            exit 0;
-        } elsif($arg =~ /(-u|--uninstall)/){
-            my $abs_path = $ENV{'PWD'};
-            print((not (-e ($abs_path . '/setup_bashrc.pl')) and "setup script missing...\n")
-            or ((not system('perl ' . $abs_path . '/setup_bashrc.pl -u')) or "setup failed\n"));
-            exit 0;                         #end hackish code:
         } elsif($arg =~ /(-l|--langs)/){
             $langs = 1;
         } elsif($arg =~ /(-nc|--nocolors)/){
@@ -237,8 +229,8 @@ sub GetCPUInfo {
     my $buffer = ReadFile('/proc/cpuinfo');
     if($buffer) {
         $processor->{'1Vendor'} = FirstMatch($buffer, qr/vendor_id$re_cpu/m);
-        $processor->{'2Model'} = FirstMatch($buffer, qr/model name$re_cpu/m);
-        $processor->{'2Model'} =~ s/$re_intelghz//;
+        $processor->{'2Model'} = FirstMatch($buffer, qr/model name$re_cpu(?:$re_intelghz)?/m);
+        #$processor->{'2Model'} =~ s/$re_intelghz//;
         
         my $cores;
         if($buffer =~ qr/cpu cores$re_cpu/m) {
@@ -257,7 +249,7 @@ sub GetCPUInfo {
         if(-e '/sys/bus/cpu/devices/cpu0/cpufreq/bios_limit') { #more accurate
             $freq = sprintf('%0.2f', (ReadFile('/sys/bus/cpu/devices/cpu0/cpufreq/bios_limit') / 1000000))
         } else {
-            $freq =  FirstMatch($buffer, qr/cpu MHz$re_cpu/m) / 1000;
+            $freq = ($1 / 1000) if($buffer =~ qr/cpu MHz$re_cpu/m);#FirstMatch($buffer, qr/cpu MHz$re_cpu/m) / 1000;
             $freq = sprintf('%0.2f', $freq);
         }
         $processor->{'3Details'} .= ($processor->{'3Details'} ? '@' : '') . $freq . 'GHz ' if($freq);
@@ -283,22 +275,22 @@ my $re_anyword = eval { qr/(.+)/ };
 sub GetMoboInfo {
     my $buffer = ReadFile('/sys/class/dmi/id/board_vendor');
     if($buffer) {
-        $motherboard->{'1Vendor'} = FirstMatch($buffer, $re_anyword);
+        $motherboard->{'1Vendor'} = $1 if($buffer =~ /$re_anyword/);
         undef $buffer;
     }
     $buffer = ReadFile('/sys/class/dmi/id/board_name');
     if($buffer) {
-        $motherboard->{'2Board'} = FirstMatch($buffer, $re_anyword);
+        $motherboard->{'2Board'} = $1 if($buffer =~ /$re_anyword/);
         undef $buffer;
     }
     $buffer = ReadFile('/sys/class/dmi/id/bios_vendor');
     if($buffer) {
-        $motherboard->{'3Bios'} = FirstMatch($buffer, $re_anyword);
+        $motherboard->{'3Bios'} = $1 if($buffer =~ /$re_anyword/);
         undef $buffer;
     }
     $buffer = ReadFile('/sys/class/dmi/id/bios_version');
     if($buffer) {
-        $motherboard->{'3Bios'} .= ' (' . FirstMatch($buffer, $re_anyword) . ')';
+        $motherboard->{'3Bios'} = "$motherboard->{'3Bios'} ($1)" if($buffer =~ /$re_anyword/);
         undef $buffer;
     }
 }
@@ -356,8 +348,8 @@ my %desktops = (
 
 my $os = {
     '1Distro' => undef,
-    '3User@Host' => undef,
     '2Kernel' => undef,
+    '3User@Host' => undef,
     '4WM/DE' => undef,
     '5Packages' => undef,
 };
@@ -365,16 +357,16 @@ my $os = {
 sub GetOSInfo {
     my $buffer = ReadFile('/proc/version');
     if($buffer) {
-        $os->{'2Kernel'} = FirstMatch($buffer, qr/^([\w]+) version /im) . ' ' . FirstMatch($buffer, qr/version $re_version/im);
+        $os->{'2Kernel'} = ($buffer =~ qr/^([\w]+) version /im ? "$1 " : ' ') . ($buffer =~ qr/version $re_version/im ? "$1" : '');
         undef $buffer;
     }
     
-    if(not ($os->{'3User@Host'} = $ENV{'USER'})) {
+    if(not ($os->{'3User@Host'} = $ENV{'USER'})) { #if USER env variable not present fallback to whoami
         $os->{'3User@Host'} = FirstMatch(`whoami`, $re_anyword) if (CommithForth('whoami'));
     }
     if(-e '/proc/sys/kernel/hostname') { #much faster
         chomp ($os->{'3User@Host'} .= "@" . ReadFile('/proc/sys/kernel/hostname'));
-    } elsif(CommithForth('hostname') and `hostname` =~ $re_anyword) {
+    } elsif(CommithForth('hostname') and `hostname` =~ $re_anyword) { #fallback to hostname
         $os->{'3User@Host'} .= ($1 ? "\@$1" : '');
     }
     
@@ -384,7 +376,7 @@ sub GetOSInfo {
             $os->{'1Distro'} .= ' ' . FirstMatch($buffer, qr/DISTRIB_RELEASE=$re_anyword/m) . ' ' . FirstMatch($buffer, qr/DISTRIB_CODENAME=$re_anyword/m);
             undef $buffer;
         }
-    } elsif((my $line =(grep(/([\w]+-release)$/, `ls -1 /etc/*-release 2>&1`))[0])) { #propose: /([\w]+-release|[\w]+_version)$/
+    } elsif((my $line =(grep(/([\w]+-release|[\w]+_version)$/, `ls -1 /etc/*-release 2>&1`))[0])) { #propose: /([\w]+-release|[\w]+_version)$/
         if($line =~ $re_anyword) {
             my $matching_file = $1;
             if(-e -r $matching_file) {
@@ -435,11 +427,7 @@ sub GetOSInfo {
                         last;
                     }
                 }
-                if($os->{'4WM/DE'}) {
-                    $os->{'4WM/DE'} .= "/$desktops{$desktop}";
-                } else {
-                    $os->{'4WM/DE'} = $desktops{$desktop};
-                }
+                $os->{'4WM/DE'} = ($os->{'4WM/DE'} ? "$os->{'4WM/DE'}\/" : '') . "$desktops{$desktop}";
             }
             undef @plist;
         }
@@ -496,13 +484,6 @@ my $gpu = {
     '3Driver' => undef,
 };
 
-# my @driver_patterns = ( #needs proper patterns...
-#     qr/X($re_version-$re_version)/i,
-#     qr/(mesa $re_version)/i,
-#     qr/(nvidia $re_version)/i,
-#     qr/$re_version/,
-# );
-
 sub GetGPUInfo {
     if(-e '/proc/driver/nvidia/gpus/0/information') {
         my $contents = ReadFile '/proc/driver/nvidia/gpus/0/information';
@@ -510,29 +491,6 @@ sub GetGPUInfo {
         $gpu->{'2Model'} = $1 if ($contents =~ /Model:[\.\s]+(.+)/i);
         $gpu->{'3Driver'} = $1 if ( (ReadFile '/proc/driver/nvidia/version') =~ /Module[\s]+$re_version[\s]/i);
     } 
-#     elsif(CommithForth('glxinfo')) {
-#         my @glx_data = `glxinfo`;
-#         
-#         $gpu->{vendor} = (grep(/OpenGL vendor string/, @glx_data))[0];
-#         $gpu->{vendor} = $1 if($gpu->{vendor} =~ qr/\:\ ([\w\.\ ]+)/);
-#         
-#         $gpu->{driver} = ((grep(/OpenGL core profile version string/, @glx_data))[0] or (grep(/OpenGL version string/, @glx_data))[0]);
-#         if($gpu->{driver}) {
-#             foreach my $regex(@driver_patterns) { #yes, I replaced a smartmatch with a foreach loop, forgive me.
-#                 if($gpu->{driver} =~ $regex) {
-#                     $gpu->{driver} = $1;
-#                     last; #in the words of shaggy, "lets get outta here!"
-#                 }
-#             }
-#         }
-#         
-#         $gpu->{card} = (grep(/OpenGL renderer string/, @glx_data))[0];
-#         if($gpu->{card}) {
-#             $gpu->{card} = $1 if($gpu->{card} =~ qr/:[\s]+([^\/\n]+)[\/]?/);
-#         }
-#         
-#         undef @glx_data;
-#     }
 }
 
 #==========================
@@ -590,8 +548,8 @@ GetGPUInfo();
 PrintHashTable($os, 'Operating System');
 PrintHashTable($motherboard, 'Motherboard');
 PrintHashTable($processor, 'Processor');
-PrintHashTable($memory, 'Memory');
 PrintHashTable($gpu, 'Video Card');
+PrintHashTable($memory, 'Memory');
 
 PrintHashes() if($langs);
 
